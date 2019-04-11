9a5c5171070b7a9f4c0c68d52618d3dd013d106b
Bruno.Silva <bruno_m_c_silva@hotmail.com>
2019-04-11 22:55:13 +0100

    Added Authentication with Volley.

--- app/src/main/java/pt/gsdt/stationpurchaseapp/Login_Activity.java
+++ app/src/main/java/pt/gsdt/stationpurchaseapp/Login_Activity.java
@@ -1,8 +1,10 @@
 package pt.gsdt.stationpurchaseapp;
 
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -23,7 +25,21 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.net.CookieHandler;
+import java.net.CookieManager;
+import java.net.CookiePolicy;
+
 public class Login_Activity extends AppCompatActivity {
+
+    private static final String PREFS_NAME = "preferences";
+    private static final String PREF_UNAME = "Username";
+    private static final String PREF_PASSWORD = "Password";
+
+    private final String DefaultUnameValue = "";
+    private String UnameValue;
+
+    private final String DefaultPasswordValue = "";
+    private String PasswordValue;
 
     private EditText etUsername;
     private EditText etPassword;
@@ -43,8 +59,8 @@ public class Login_Activity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                sendMessage();
-
+                //sendMessage();
+                signInUser();
 
             }
 
@@ -52,7 +68,49 @@ public class Login_Activity extends AppCompatActivity {
 
     }
 
-    private void sendMessage() {
+    @Override
+    public void onPause() {
+        super.onPause();
+        savePreferences();
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        loadPreferences();
+    }
+
+    private void savePreferences() {
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME,
+                Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+
+        // Edit and commit
+        UnameValue = etUsername.getText().toString();
+        PasswordValue = etPassword.getText().toString();
+        System.out.println("onPause save name: " + UnameValue);
+        System.out.println("onPause save password: " + PasswordValue);
+        editor.putString(PREF_UNAME, UnameValue);
+        editor.putString(PREF_PASSWORD, PasswordValue);
+        editor.commit();
+    }
+
+    private void loadPreferences() {
+
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME,
+                Context.MODE_PRIVATE);
+
+        // Get value
+        UnameValue = settings.getString(PREF_UNAME, DefaultUnameValue);
+        PasswordValue = settings.getString(PREF_PASSWORD, DefaultPasswordValue);
+        etUsername.setText(UnameValue);
+        etPassword.setText(PasswordValue);
+        System.out.println("onResume load name: " + UnameValue);
+        System.out.println("onResume load password: " + PasswordValue);
+    }
+
+    /*private void sendMessage() {
 
         String user_text = etUsername.getText().toString();
         String pass_text = etPassword.getText().toString();
@@ -61,12 +119,15 @@ public class Login_Activity extends AppCompatActivity {
         i.putExtra("Pass", pass_text);
         startActivity(i);
 
-    }
+    }*/
 
     private void signInUser() {
 
         String user = etUsername.getText().toString();
         String pass = etPassword.getText().toString();
+
+        CookieManager cookieManager = new CookieManager(new PersistentCookieStore(mContext), CookiePolicy.ACCEPT_ORIGINAL_SERVER);
+        CookieHandler.setDefault(cookieManager);
 
         RequestQueue rq;
         Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024);
@@ -74,36 +135,35 @@ public class Login_Activity extends AppCompatActivity {
         rq = new RequestQueue(cache, nw);
         rq.start();
 
-        String appID = "b6948414a6f42be3075eee653436b668";
-        JSONObject usersignInCred = new JSONObject();
-        String reqDate = "2019-04-10T15:14:00";
-        String reqID = "1";
-
-        JSONObject reqAuthenticator = new JSONObject();
+        String ApplicationID = "b6948414a6f42be3075eee653436b668";
+        JSONObject Credentials = new JSONObject();
+        String RequestDate = "2019-04-10T15:14:00";
+        String RequestID = "1";
+        JSONObject RequestAuthentication = new JSONObject();
 
         try {
 
+            RequestAuthentication.put("ApplicationID", ApplicationID);
+            Credentials.put("User", user);
+            Credentials.put("Password", pass);
+            RequestAuthentication.put("Credentials", Credentials);
+            RequestAuthentication.put("RequestDate", RequestDate);
+            RequestAuthentication.put("RequestID", RequestID);
 
-            reqAuthenticator.put("ApplicationID", appID);
-            usersignInCred.put("User", user);
-            usersignInCred.put("Password", pass);
-            reqAuthenticator.put("Credentials", usersignInCred);
-            reqAuthenticator.put("RequestDate", reqDate);
-            reqAuthenticator.put("RequestID", reqID);
-
-            JsonObjectRequest jOR = new JsonObjectRequest(Request.Method.POST, getString(R.string.sign_in_url), reqAuthenticator, new Response.Listener<JSONObject>() {
+            JsonObjectRequest jOR = new JsonObjectRequest(Request.Method.POST, getString(R.string.sign_in_url), RequestAuthentication, new Response.Listener<JSONObject>() {
                 @Override
-                public void onResponse(JSONObject resAuthentication) {
-                    Log.d("Resposta", resAuthentication.toString());
+                public void onResponse(JSONObject ResponseAuthentication) {
+                    Log.d("Resposta", ResponseAuthentication.toString());
+
                     try {
 
-                        if (resAuthentication.get("Message").equals("")) {
+                        if (!ResponseAuthentication.get("AuthorizationToken").equals("")) {
+
                             Intent i = new Intent(getApplicationContext(), MainActivity.class);
-                            /*i.putExtra("User", etUsername.getText().toString());
-                            i.putExtra("Pass", etPassword.getText().toString());*/
                             startActivity(i);
                             finish();
                             Toast.makeText(Login_Activity.this, R.string.welcome, Toast.LENGTH_SHORT).show();
+
                         } else {
                             Toast.makeText(Login_Activity.this, R.string.error_login, Toast.LENGTH_SHORT).show();
                         }
